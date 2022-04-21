ce2ab64e96ed826a28beb61e58ed5c031eee285f
Esteban Q <equesadal@gmail.com>
2022-04-21 14:35:53 -0600

    Identation fix

--- src/main/java/com/example/lambdatest/controllers/LambdaTestController.java
+++ src/main/java/com/example/lambdatest/controllers/LambdaTestController.java
@@ -12,17 +12,17 @@ public class LambdaTestController {
 
 	@PostMapping(path = "/lambdatest", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
 	public String LambdaTest(@RequestBody String requestBody) {
-        OperationResolver resolver = new OperationResolver();
-        OperationInputParser parser = new OperationInputParser();
-        StringBuilder output = new StringBuilder();
-        
-        ArrayList<OperationInput> inputs = parser.Parse(requestBody); // I didnt add sanizitation for the requestBody for now, in prod that is a must.
+		OperationResolver resolver = new OperationResolver();
+		OperationInputParser parser = new OperationInputParser();
+		StringBuilder output = new StringBuilder();
 
-        System.out.println("Starting to process");
-        for(OperationInput input:inputs) {
-            String result = resolver.Resolve(input.operationType, input.number);
-            output.append(result + "\n");
-        }
+		ArrayList<OperationInput> inputs = parser.Parse(requestBody); // I didnt add sanizitation for the requestBody for now, in prod that is a must.
+
+		System.out.println("Starting to process");
+		for(OperationInput input:inputs) {
+		    String result = resolver.Resolve(input.operationType, input.number);
+		    output.append(result + "\n");
+		}
 
 		return output.toString();
 	}
